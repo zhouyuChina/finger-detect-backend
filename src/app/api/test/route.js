344caa6bbcf5a2aee8ec81df74124d1fdcf2b31a
zhouyu <263203788@qq.com>
2025-08-03 04:51:23 +0800

    fix: 修复测试接口Prisma客户端导入路径

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../../generated/prisma/index.js'
 
 const prisma = new PrismaClient()
 
