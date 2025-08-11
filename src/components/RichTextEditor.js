dd04d5285ee49e64a20ca8097a646a9da0161b10
zhouyu <263203788@qq.com>
2025-08-11 19:45:05 +0800

    feat: 配置TinyMCE中文语言包，所有弹窗和界面显示中文

--- src/components/RichTextEditor.js
+++ src/components/RichTextEditor.js
@@ -90,7 +90,9 @@ export default function RichTextEditor({ value, onChange, placeholder = "请输�
         text-decoration: underline;
       }
     `,
+    // 中文语言配置
     language: 'zh_CN',
+    language_url: 'https://cdn.tiny.cloud/1/wv5ht0amieanuflleqiwvr1jq0sqhy6einysmxoak9jntoa0/tinymce/6.8.3/langs/zh_CN.js',
     placeholder: placeholder,
     branding: false,
     elementpath: false,
